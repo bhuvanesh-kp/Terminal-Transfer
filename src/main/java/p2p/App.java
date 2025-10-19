ad6fbdc04cb902c3e03e2fbfba68036ad177b6db
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2025-10-19 11:03:15 +0530

    updated

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -1,10 +1,29 @@
 package p2p;
 
-/**
- * Hello world!
- */
+import p2p.controller.FileController;
+
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            FileController fileController = new FileController(8000);
+            fileController.start();
+
+            System.out.println("Terminal-Transfer started in port : 8080");
+            System.out.println("UI available at port : 3000");
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down Terminal-Transfer ...");
+                fileController.stop();
+            }));
+
+            System.out.println("Press Enter to stop the server");
+            System.in.read();
+        }
+        catch (IOException e){
+            System.err.println("Error starting the server " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
